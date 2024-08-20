f59a8baa8bd40990b1ec4f6d3e67b27fff9ebecc
aadd5533 <aadd55553333@gmail.com>
2024-08-20 23:09:58 +0800

    Update 2页.js

--- 2页.js
+++ 2页.js
@@ -1,11 +1,1 @@
 let targetPackageName = "com.sup.android.superb";  // 将这个替换为你想检测的应用包名
-
-// 获取当前运行的应用包名
-let currentApp = currentPackage();
-
-// 判断当前运行的应用是否为目标应用
-if (currentApp === targetPackageName) {
-    toast("目标应用正在运行");
-} else {
-    toast("目标应用未在运行");
-}
